1f242a7126f9a24f347a8b4ffaca7110cd147291
Navasalf <137118846+Navasalf@users.noreply.github.com>
2024-09-20 12:20:19 -0300

    ejercicios listo

---
+++ exercisesclass1.py
@@ -0,0 +1,37 @@
+# 1.- Desafio de Numeros
+
+# partidos_ganados = int(input()) * 3
+# partidos_empatados = int(input()) * 1
+# partidos_perdidos = int(input()) * 0
+# puntuacion = partidos_ganados + partidos_empatados + partidos_perdidos
+# print(puntuacion / 20)
+
+
+
+
+# 2.- Desafio Slicing
+
+# cadena = "acitametaM ,5.8 ,otipeP ordeP"
+
+# cadena_volteada = cadena[::-1]
+# nombre_alumno =  cadena_volteada[0:12] 
+# nota = cadena_volteada[14:17]
+# materia = cadena_volteada[19:29]
+
+# print (cadena_volteada)
+# print(nombre_alumno)
+# print(nota)
+# print(materia)
+# print(f"{nombre_alumno} ha sacado {nota} en la materia de {materia}")
+
+
+
+
+# .-3
+
+# nota_1 = float(input("ingrese la nota 1:  "))
+# nota_2 = float(input("ingrese la nota 2:  "))
+# nota_3 = float(input("ingrese la nota 3:  "))
+# nota_final = (nota_1 * 0.2) + (nota_2 * 0.3) + (nota_3 * 0.50)
+
+# print(f"la nota final es de: {nota_final}")
