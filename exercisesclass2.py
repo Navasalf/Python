f0f7d15e4a23372ccec8df0de3196e2189741cde
Navasalf <137118846+Navasalf@users.noreply.github.com>
2024-09-23 09:57:47 -0300

    second class exercises

---
+++ exercisesclass2.py
@@ -0,0 +1,47 @@
+# 1.- Desafio de Listas
+
+# lista_1 = ["Alfredo", "Navas", 31, "Sarahis", "Velis", 27]
+# lista_2 = ["David", "Vargas", 18, "Del Carmen", "Soptee", 12]
+
+
+
+# lista_1.append("3242342")
+# lista_1.append("Hola Mundo")
+
+
+# lista_2.append("Hola y Adios")
+# lista_2.append(5555)
+
+# lista_3 = lista_1.copy()
+# lista_3.pop()
+
+# lista_4 = lista_2.copy()
+# lista_4.pop()
+# lista_4.pop(0)
+
+# lista_5 = lista_3.copy() + lista_4.copy()
+
+# print(lista_1)
+# print(lista_2)
+# print(lista_3)
+# print(lista_4)
+# print(lista_5) 
+
+
+# DESAFIO DE TUPLAS
+
+# tupla = (8, 15, 4, 39, 5, 89, 87, 19, 7, -755, 88, 2123, 2, 11, 15, 9, 355)
+
+# indice = tupla.index(87)
+# cuenta = tupla.count(7)
+# longitud_tupla = len(tupla)
+
+# inicio = longitud_tupla -3
+# ultimos_tres = list(tupla[inicio:])
+
+
+# print(tupla[16])
+# print(indice)
+# print(cuenta)
+# print(longitud_tupla)
+# print(ultimos_tres)
