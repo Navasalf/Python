fe8686a000646b06332d1f684f96e4bb82a7d65e
Navasalf <137118846+Navasalf@users.noreply.github.com>
2024-09-25 20:29:26 -0300

    third class exercises

---
+++ exercisesclass3.py
@@ -0,0 +1,26 @@
+# EXERCIESCONJUNTOS
+
+# colores = {"rojo", "blanco", "azul"}
+# print(colores)
+
+# colores.update(["violeta", "dorado"])
+# # colores.add("violeta")
+# # colores.add("dorado")
+# print(colores)
+
+# colores.discard("celeste")
+# colores.discard("blanco")
+# colores.discard("dorado")
+# print(colores)
+
+
+# EXERCIESDICTIONARIO
+
+copa_mundial = {1990: "alemania", 1994: "Brasil", 1998: "Francia", 2002: "Brasil", 2006: "Italia", 2010: "España", 2014: "Alemania", 2018: "Francia", 2022: "Argentina"}
+year = int(input("ingrese el año del cual le gustaria saber quien fue el campeon mundial: "))
+
+if year in copa_mundial:
+    print("el campeon del año", year, "fue", copa_mundial[year])
+else:
+    print("No hay registro de ese año o en ese año no se ha jugado la copa mundial")
+
